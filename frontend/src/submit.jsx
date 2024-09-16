af3c51e770e436d62b637afc851dd0bc107fb5a3
aadarsh-nagrath <anagrath1@gmail.com>
2024-09-16 22:28:02 +0530

    initial config

--- frontend/src/submit.jsx
+++ frontend/src/submit.jsx
@@ -1,10 +1,11 @@
 // submit.js
+import { Button } from "@/components/ui/button";
 
 export const SubmitButton = () => {
 
     return (
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <button type="submit">Submit</button>
+            <Button variant="destructive" type="submit">Submit</Button>
         </div>
     );
 }
